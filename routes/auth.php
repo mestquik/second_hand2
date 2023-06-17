d6bd1e609458219d0433e4020dec2b7644d102d9
mestquik <121771305+mestquik@users.noreply.github.com>
2023-06-17 13:48:20 +0300

    Giriş/Çıkış

--- routes/auth.php
+++ routes/auth.php
@@ -17,7 +17,6 @@ use Illuminate\Support\Facades\Route;
     Route::post('register', [RegisteredUserController::class, 'store'])->name('register_post');
 
 
-
     Route::get('/login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
 
